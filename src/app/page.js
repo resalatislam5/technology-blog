d9f5f5485ab91ce47c73589bda6927bbde62ca0f
Resalat islam <resalatislam5@gmail.com>
2023-07-27 21:12:41 +0600

    banner section

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-import Banner from "./home/banner";
+import Banner from "./home/Banner";
 
 export default function Home() {
   return (
-    <div>
+    <div className="max-w-7xl mx-auto px-5">
         <Banner />
     </div>
   )
